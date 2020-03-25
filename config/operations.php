abf8dae403b07d8c7b3a245e3823b99226e5ee83
Aya89519 <62358771+Aya89519@users.noreply.github.com>
2020-03-25 19:25:53 +0000

    insert data (#13)

--- config/operations.php
+++ config/operations.php
@@ -6,6 +6,28 @@
 
     class operations extends dbconfig
     {
+        // Insert Record in the Database
+        public function Store_Record()
+        {
+            global $db;
+            if(isset($_POST['btn_save']))
+            {
+                $name = $db->check($_POST['name']);
+                $email = $db->check($_POST['email']);
+                $phone = $db->check($_POST['phone']);
+                $password = $db->check($_POST['password']);
+
+                if($this->insert_record($name,$email,$phone,$password))
+                {
+                    //echo "<script>window.location.href='login_user.php'</script>";
+                    header("Location:login_user.php");
+                }
+                else
+                {
+                    echo '<div class="alert alert-danger"> Failed </div>';
+                }
+            }
+        }
         
     } 
 ?>
