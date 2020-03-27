91ddc1b728caf06777ad2223d8c1aac8ed6ac7fc
Mariomamamdouh <38222805+Mariomamamdouh@users.noreply.github.com>
2020-03-27 16:40:56 +0000

    Revert "View users (#33)"
    This reverts commit 1a4f938046f4f14e56d4f8ff759f8c1b6e8086e9.

--- view.php
+++ view.php
@@ -23,31 +23,6 @@
                   $db->display_message(); 
                   $db->display_message();
             ?>
-           <table Id="customers">
-                <tr>
-                    <th style="width: 10%;font-size:20px"> ID </th>
-                    <th style="width: 10%;font-size:20px"> Name </th>
-                    <th style="width: 20%;font-size:20px"> Email </th>
-                    <th style="width: 20%;font-size:20px"> Phone </th>
-                    <th style="width: 20;font-size:20px" colspan="2">Operations</th>
-                </tr>
-                <tr>
-                    <?php 
-                        while($data = mysqli_fetch_assoc($result))
-                        {
-                    ?>
-                        <td><?php echo $data['id'] ?></td>
-                        <td><?php echo $data['name'] ?></td>
-                        <td><?php echo $data['email'] ?></td>
-                        <td><?php echo $data['phone'] ?></td>
-  <td style="border-radius: 8px;"><a href="  edit.php?U_ID=<?php echo $data['id'] ?>" class="button">Edit</a></td>
-                        <td><a href="del.php?D_ID=<?php echo $data['id'] ?>" class="button1">Del</a></td>
-                </tr>
-                <?php
-                        }
-                    ?>
-                        </table>
-                    </div>
  </body>
 </html>
 
