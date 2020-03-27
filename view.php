e19d09e8a7b1f7ad1c31fc3efc9a7b9268161a2e
Mariomamamdouh <38222805+Mariomamamdouh@users.noreply.github.com>
2020-03-27 15:40:09 +0000

    Update view.php

--- view.php
+++ view.php
@@ -5,3 +5,22 @@
     $db = new operations();
     $result=$db->view_record();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css\s.css">
+    <title></title>
+</head>
+
+<body>
+        <div>
+            <h2 class="head"> All Users </h2>
+        </div>
+        <div class="card-body">
+            <?php
+                  $db->display_message(); 
+                  $db->display_message();
+            ?>
+ </body>
+</html>
