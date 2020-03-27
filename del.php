5e1537bc2a239f0a3e771af7f8f10671644acefd
Mona Fouad <62349952+mona564@users.noreply.github.com>
2020-03-27 21:29:26 +0000

    Delete (#36)
    * delete record
    
    * delete page
    
    * Update del.php

--- del.php
+++ del.php
@@ -2,6 +2,14 @@
 
     require_once('./config/dbconfig.php');
     $db = new operations();
+
+     if(isset($_GET['D_ID']))
+    {
+        global $db;
+        $ID = $_GET['D_ID'];
     
     
 ?>
+
+    
+    
