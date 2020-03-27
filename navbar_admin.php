7723e80e5a4193363c2181301d55bdec639bea5f
Nesma999 <62080937+Nesma999@users.noreply.github.com>
2020-03-27 15:44:22 +0000

    navbar admin

---
+++ navbar_admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css\navbar.css">
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="Home.php">Home</a></li>
+  <li><a href="view.php">View users</a></li>
+  <li><a href="">Interview online </a></li>
+  <li><a href="search.php">Applicant Report  </a></li>
+  <li><a href="">Result Report</a></li>
+  <li><a href="">Feedback</a></li>
+  <li><a href="logout.php">Logout</a></li>
+</ul>
+
+</body>
+</html>
