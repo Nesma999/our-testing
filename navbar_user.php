334246ca1741fe920028106f5d432ff664d1813f
Haidy-hamdy <62071573+Haidy-hamdy@users.noreply.github.com>
2020-03-28 22:52:14 +0000

    Navbar (#52)
    * navbar admin
    
    * navbar user
    
    * Update navbar_user.php
    
    * Update navbar_user.php
    
    Co-authored-by: Nesma999 <62080937+Nesma999@users.noreply.github.com>
    Co-authored-by: Aya89519 <62358771+Aya89519@users.noreply.github.com>

--- navbar_user.php
+++ navbar_user.php
@@ -9,5 +9,10 @@
 <ul>
   <li><a class="active" href="Home.php">Home</a></li>
 
+  <li><a href="traveling_information.php">Traveling Information</a></li>
+  <li><a href="applicant_information.php">Applicant Information</a></li>
+ 
+
+
 </html>
 
