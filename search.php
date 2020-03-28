f75091914bef16fcd244b4b122a1e209287cf129
Nesma999 <62080937+Nesma999@users.noreply.github.com>
2020-03-28 20:42:23 +0000

    Update search.php

--- search.php
+++ search.php
@@ -24,4 +24,10 @@ include 'navbar_admin.php';
   margin-left: 22px;
 }
 
+#customers th,#customers td {
+   padding: 20px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+  border: 1px solid #ddd;
 
+}
