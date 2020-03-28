9f1c6c73825a7058ffaa9b9c557425301480e698
Nesma999 <62080937+Nesma999@users.noreply.github.com>
2020-03-28 21:17:54 +0000

    Update search.php

--- search.php
+++ search.php
@@ -47,6 +47,22 @@ include 'navbar_admin.php';
   margin-right: 700px;
   margin-left: 50px;
 }
-
+.input{
+  width: 20%;
+  height: 50px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 20px;
+  background-color: white;
+  padding: 40px;
+  transition: width 0.4s ease-in-out;
+   margin-top: 130px;
+  margin-bottom: 40px;
+  margin-right: 150px;
+  margin-left: 500px;
+}
+    </style>
+    </head>
 
 
