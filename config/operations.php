e30689e19b83350520b9b7200df0b40cd739ac05
Mariomamamdouh <38222805+Mariomamamdouh@users.noreply.github.com>
2020-03-30 13:29:11 +0100

    Update operations.php

--- config/operations.php
+++ config/operations.php
@@ -153,7 +153,21 @@
                
             }
         }
-
+       // Update Function 2
+        public function update_record($id,$name,$email,$phone)
+        {
+            global $db;
+            $sql = "update users set name='$name', email='$email', phone='$phone' where id='$id'";
+            $result = mysqli_query($db->connection,$sql);
+            if($result)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
 
         
