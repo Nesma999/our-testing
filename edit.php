c52fdc01267342dcfda6dae10f49068b741e5993
Mariomamamdouh <38222805+Mariomamamdouh@users.noreply.github.com>
2020-03-30 18:01:43 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -13,5 +13,26 @@
      <link rel="stylesheet" type="text/css" href="css\s.css">
     <title></title>
 </head>
+<body>
+        <?php $db->Store_Record(); ?>
+        <div class="box">
+      <h2> Edit </h2>
+            <form method="POST">
+                 <div class="inputBox">
+                <input type="hidden" name="id" value="<?php echo $data['id']; ?>">
+            </div>
+             <div class="inputBox">
+                <input type="text" name="name" placeholder="Name" class="form-control mb-2" required value="<?php echo $data['name']; ?>">
+            </div>
+             <div class="inputBox">
+                <input type="text" name="email" placeholder=" E-mail" class="form-control mb-2" required value="<?php echo $data['email']; ?>">
+            </div>
+             <div class="inputBox">
+                <input type="text" name="phone" placeholder=" Phone" class="form-control mb-2" required value="<?php echo $data['phone']; ?>">
+        </div>
+            <button class="btn btn-success" name="btn_update"> Update </button>
+        </form>
+                    </div>
+</body>
 
 </html>
