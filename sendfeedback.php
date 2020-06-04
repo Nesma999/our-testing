c13a2d5ba82c49f35618aa619dc3002670185466
Aya89519 <62358771+Aya89519@users.noreply.github.com>
2020-06-04 21:29:56 +0200

    Update sendfeedback.php (#66)

--- sendfeedback.php
+++ sendfeedback.php
@@ -16,4 +16,28 @@
 	<link rel="stylesheet" type="text/css" href="css\s.css">
   <link rel="stylesheet" href="css\footer.css">
 </head>
+	<body>			
+	<div class="box">
+      <h2> Send Feedback </h2>
+        <form method="POST">
+           <div class="inputBox">
+               <input type="text" name="id" placeholder="ID" required>
+           </div>
+            <div class="inputBox">
+              <input type="text" name="feedback" placeholder="Feedback" required>
+           </div>
+                    
+              <button class="btn btn-success" name="send"> Send </button>
+        </form>
+     </div>
+				
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+
+</body>
 </html>
