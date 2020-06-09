5312a0c60af79ada0e9bb4546ab26e4e0fa888ed
Mona Fouad <62349952+mona564@users.noreply.github.com>
2020-06-09 16:56:39 +0200

    Update navbar_admin.php (#76)

--- navbar_admin.php
+++ navbar_admin.php
@@ -8,7 +8,7 @@
 <ul>
   <li><a class="active" href="Home.php">Home</a></li>
   <li><a href="view.php">View users</a></li>
-  <li><a href="">Interview online </a></li>
+  <li><a href="traveling_information.php">Interview online </a></li>
   <li><a href="search.php">Applicant Report  </a></li>
   <li><a href="">Result Report</a></li>
  <li><a href="feedback.php">Feedback</a></li>
