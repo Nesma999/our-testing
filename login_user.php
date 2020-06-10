6d32c4f610d51728842bd493365ea0371ac0362e
Nesma999 <62080937+Nesma999@users.noreply.github.com>
2020-06-10 23:02:26 +0200

    Update login_user.php

--- login_user.php
+++ login_user.php
@@ -38,7 +38,7 @@ echo "<script>window.location.href='login_user.php'</script>";
 <body>
   <ul>
   <li><a class="active" href="Home.php">Home</a></li>
-  <li><a href="">About Egypt</a></li>
+  <li><a href="index.php">About Egypt</a></li>
   <li style="float:right"><a href="login_user.php">Signin</a></li>
   <li style="float:right"><a href="register.php">Signup</a></li>
 </ul>
