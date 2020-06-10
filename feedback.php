10f14d0dbcb0ab15432c8ae4489d31e7ba6304d6
Haidy-hamdy <62071573+Haidy-hamdy@users.noreply.github.com>
2020-06-10 20:01:13 +0200

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -2,7 +2,7 @@
     
     require_once('./config/dbconfig.php'); 
     include 'navbar_admin.php';
-    include('footer.php');
+    //include('footer.php');
     $db = new operations();
     $result=$db->view_feedback();
 ?>
